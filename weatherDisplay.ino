ab61fc754c004e1d30b5d0f569591a78aa6e3770
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2024-06-09 17:37:19 -0400

    get weather data from API

--- weatherDisplay.ino
+++ weatherDisplay.ino
@@ -1,6 +1,4 @@
-#include <WiFi.h>
-const char* ssid = "meem";      // Replace with your network SSID (name)
-const char* password = "6478539650";  // Replace with your network password
+#include "weatherDisplay.h"
 
 void connectToWifi() {
   // Connect to Wi-Fi
@@ -19,12 +17,34 @@ void connectToWifi() {
   Serial.println(WiFi.localIP());
 }
 
+void fetchWeatherData() {
+  if (WiFi.status() == WL_CONNECTED) { // Check if the device is connected to Wi-Fi
+    HTTPClient http;
+    http.begin(WEATHER_URL); // Specify the URL
+    int httpResponseCode = http.GET(); // Make the request
+
+    if (httpResponseCode > 0) { // Check for a successful response
+      String payload = http.getString();
+      Serial.println("Weather data:");
+      Serial.println(payload); // Print the response payload
+    } else {
+      Serial.print("Error on HTTP request: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end(); // Free resources
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);  // Start the serial communication
   delay(10);
 
   connectToWifi();
+  fetchWeatherData();
 }
 
 void loop() {
